105f4a524fde075b94eb545198f40a8c4bb2129e
Booyamando <filibs10@gmail.com>
2020-11-20 00:23:43 +0000

    first init

--- src/main.js
+++ src/main.js
@@ -6,5 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["we", "they", "our", "the", "us"];
+  let adj = ["scary", "beautiful", "nice", "yellow", "fast"];
+  let noun = ["car", "man", "people", "woman", "pen"];
+  let domainList = [];
+
+  let domain = document.querySelector("#domain");
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        // domain.innerHTML += `<li>${pronoun[i]}${adj[j]}${noun[k]}.com</li>`;
+        domainList.push(`<ul>${pronoun[i]}${adj[j]}${noun[k]}.com</ul>`);
+      }
+    }
+  }
+  domain.innerHTML = domainList.join("");
 };
